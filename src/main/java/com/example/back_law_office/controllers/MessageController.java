5d3e27fadc78c38f05328701e121826e02e9dc6d
korpi-zip <santiescoal@gmail.com>
2025-05-15 19:40:20 -0500

    Remove unnecessary blank lines in MessageController

--- src/main/java/com/example/back_law_office/controllers/MessageController.java
+++ src/main/java/com/example/back_law_office/controllers/MessageController.java
@@ -9,8 +9,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
-
 //Controlador de prueba para enviar mensajes usando RabbitMQ
 @RestController
 @RequestMapping("/api/messages")
